e6b119f3a94ccdf87b091fd1f33b027c236805c7
kkafi09 <afi.firda@gmail.com>
2022-12-29 00:22:22 +0700

    add: setup again react

--- src/components/Home/Navbar.js
+++ src/components/Home/Navbar.js
@@ -38,15 +38,29 @@ function Navbar() {
   };
 
   return (
-    <div className="navbar ">
+    <div className="navbar">
       <div className="navbar-start">
         <div className="dropdown">
           <label tabIndex={0} className="btn btn-ghost lg:hidden">
-            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" />
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h8m-8 6h16"
+              />
             </svg>
           </label>
-          <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+          <ul
+            tabIndex={0}
+            className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
+          >
             <li>
               <a>Beranda </a>
             </li>
@@ -61,7 +75,9 @@ function Navbar() {
             </li>
           </ul>
         </div>
-        <a className="btn btn-ghost normal-case text-xl font-semibold ">Dewi Vtour</a>
+        <a className="btn btn-ghost normal-case text-xl font-semibold ">
+          Dewi Vtour
+        </a>
       </div>
       <div className="navbar-center hidden lg:flex">
         <ul className="menu menu-horizontal px-1">
@@ -82,11 +98,17 @@ function Navbar() {
 
       <div className="navbar-end">
         {localStorage.getItem("loginInfo") === null ? (
-          <a className="btn rounded-full px-8 px bg-blueButton border-none mx-24 " onClick={handleSignIn}>
+          <a
+            className="btn rounded-full px-8 px bg-blueButton border-none mx-2"
+            onClick={handleSignIn}
+          >
             Masuk
           </a>
         ) : (
-          <div className="p-4 bg-blueButton text-white font-semibold rounded-full cursor-pointer" onClick={handleLogOut}>
+          <div
+            className="p-4 bg-blueButton text-white font-semibold rounded-full cursor-pointer"
+            onClick={handleLogOut}
+          >
             {jsonData.user.displayName}
           </div>
         )}
