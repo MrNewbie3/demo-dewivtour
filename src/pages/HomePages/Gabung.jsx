5604a165c7624d605842373056ed14d275c2b48d
MrNewbie3 <atharafiaffandy@gmail.com>
2023-03-06 09:12:31 +0700

    features: New Register Page

--- src/pages/HomePages/Gabung.jsx
+++ src/pages/HomePages/Gabung.jsx
@@ -2,7 +2,7 @@ function Gabung() {
   return (
     <div className="mx-auto w-5/6 lg:w-3/4 my-32 text-center">
       <p className="text capitalize font-bold text-4xl md:text-5xl leading-loose">ingin tempat wisata anda dikenal dan dikunjungi banyak orang?</p>
-      <a href="https://wa.me/6285106655664" target={"_blank"} rel="noreferrer">
+      <a href="https://register-tour.vercel.app/" target={"_blank"} rel="noreferrer">
         <button className="btn mt-12 rounded-full bg-blueButton hover:bg-blue-800 px-6 border-none shadow-xl">Gabung VRagio</button>
       </a>
     </div>
